61b81fd5c98adc0e0ac7c8537cc7ed5692fc4ca2
NghiNV <nghinv03@gmail.com>
2024-10-15 17:11:04 +0700

    Base Specification, Interface Base Repository done

---
+++ HealthcareAppointment.Data/Dtos/PaginationList.cs
@@ -0,0 +1,21 @@
+﻿namespace HealthcareAppointment.Data.Dtos
+{
+	public class PaginationList<T>
+	{
+		public ICollection<T> Items { get; }
+		public int PageNumber { get; }
+		public int PageSize { get; }
+		public int TotalPage { get; }
+		public bool IsHasNextPage => PageNumber < TotalPage;
+
+		public bool IsHasPreviousPage => PageNumber > 1;
+
+		public PaginationList(ICollection<T> items, int pageNumber, int pageSize, int totalPage)
+		{
+			Items = items;
+			PageNumber = pageNumber;
+			PageSize = pageSize;
+			TotalPage = totalPage;
+		}
+	}
+}
---
+++ HealthcareAppointment.Data/Repositories/BaseRepository/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using HealthcareAppointment.Data.Dtos;
+using HealthcareAppointment.Data.Specifications.Basespecification;
+
+namespace HealthcareAppointment.Data.Repositories.BaseRepository
+{
+	public interface IBaseRepository<T, P> where T : class
+	{
+		PaginationList<T> GetItems(IBaseSpecification<T> specification);
+		T GetById(P id);
+		T Add(T item);
+		T Update(P id, T item);
+		T Delete(P id);
+	}
+}
